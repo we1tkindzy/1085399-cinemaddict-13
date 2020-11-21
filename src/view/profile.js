264e88665537ae4a55a8696724806a6eff71c9e0
valentin74123 <58230964+valentin74123@users.noreply.github.com>
2020-11-21 21:35:05 +0500

    Создает и подключает модули view

---
+++ src/view/profile.js
@@ -0,0 +1,6 @@
+export const ctreatesProfileTemplate = () => {
+  return `<section class="header__profile profile">
+    <p class="profile__rating">Sci-Fighter</p>
+    <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+  </section>`;
+};
