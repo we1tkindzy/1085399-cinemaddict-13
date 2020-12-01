6e152a6e613b66ca8c20a613a923ad0859b8eb97
valentin74123 <58230964+valentin74123@users.noreply.github.com>
2020-12-01 19:14:28 +0500

    Создает классы для объектов

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ import FilmCardView from "./view/film-card.js";
 import LoadMoreButtonView from "./view/load-more-button.js";
 import StatisticsView from "./view/stats.js";
 import PopupView from "./view/popup.js";
-import PopupInfoView from "./view/popup-info.js";
-import PopupCommentsView from "./view/popup-comments.js";
+// import PopupInfoView from "./view/popup-info.js";
+// import PopupCommentsView from "./view/popup-comments.js";
 import {generateFilm} from "./mock/film.js";
 import {generateFilter} from "./mock/filter.js";
 import {generateComment} from "./mock/comments.js";
@@ -32,32 +32,43 @@ const siteMainElement = document.querySelector(`.main`);
 const siteFooterElement = document.querySelector(`.footer__statistics`);
 
 
-const renderPopup = (FilmListElement, film) => {
-  const filmCard = new FilmCardView(films[0])
-  const filmPopup = new PopupInfoView();
+const renderPopup = () => {
+  const popupComponent = new PopupView(films[0], comments);
+
+  const filmCardComponent = new FilmCardView(films[0]);
+  // console.log(filmCardComponent);
+  // console.log(filmCardComponent.getElement());
+  // console.log(filmCardComponent.getElement().querySelector(`.film-card__poster`));
+
+  // console.log(popupComponent.getElement());
+
+  const body = document.querySelector(`body`);
 
   const openPopup = () => {
-    FilmListElement.appendChild(new PopupView().getElement());
-  }
+    render(siteFooterElement, popupComponent.getElement(), RenderPosition.BEFOREEND);
+    siteFooterElement.appendChild(popupComponent.getElement());
+    body.classList.remove(`hide-overflow`);
+  };
 
   const closePopup = () => {
-    FilmListElement.removeChild(new PopupView().getElement());
-  }
+    siteFooterElement.removeChild(popupComponent.getElement());
+    body.classList.add(`hide-overflow`);
+  };
 
-  filmCard.getElement().querySelector(`.film-card__poster`).addEventListener(`click`, () => {
+  filmCardComponent.getElement().querySelector(`.film-card__poster`).addEventListener(`click`, () => {
     openPopup();
   });
-  filmCard.getElement().querySelector(`.film-card__title`).addEventListener(`click`, () => {
+  filmCardComponent.getElement().querySelector(`.film-card__title`).addEventListener(`click`, () => {
     openPopup();
   });
-  filmCard.getElement().querySelector(`.film-card__comments`).addEventListener(`click`, () => {
+  filmCardComponent.getElement().querySelector(`.film-card__comments`).addEventListener(`click`, () => {
     openPopup();
   });
 
-  filmPopup.getElement().querySelector(`.film-details__close-btn`).addEventListener(`click`, () => {
+  popupComponent.getElement().querySelector(`.film-details__close-btn`).addEventListener(`click`, () => {
     closePopup();
   });
-}
+};
 
 
 render(siteHeaderElement, new ProfileView().getElement(), RenderPosition.BEFOREEND);
@@ -105,7 +116,9 @@ renderTemplate(listComponent.getElement(), createsFilmsListExtraTemplate(), Rend
 
 render(siteFooterElement, new StatisticsView(countOfFilms[0]).getElement(), RenderPosition.BEFOREEND);
 
-const popupComponent = new PopupView();
-render(siteFooterElement, popupComponent.getElement(), RenderPosition.BEFOREEND);
-render(popupComponent.getElement(), new PopupInfoView(films[0]).getElement(), RenderPosition.BEFOREEND);
-render(popupComponent.getElement(), new PopupCommentsView(comments).getElement(), RenderPosition.BEFOREEND);
+// const popupComponent = new PopupView(films[0], comments);
+// renderPopup(siteFooterElement, films[0]);
+
+renderPopup();
+// render(popupComponent.getElement(), new PopupInfoView(films[0]).getElement(), RenderPosition.BEFOREEND);
+// render(popupComponent.getElement(), new PopupCommentsView(comments).getElement(), RenderPosition.BEFOREEND);
